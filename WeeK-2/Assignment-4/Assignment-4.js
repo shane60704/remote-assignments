258511c8ab3655df7a633c34fc2c5a73d08bae90
Sean <shane60704@gmail.com>
2024-06-30 02:25:18 +0800

    刪除 WeeK-2 資料夾

--- WeeK-2/Assignment-4/Assignment-4.js
+++
@@ -1,9 +0,0 @@
-function changeWords()
-{
-    div = document.getElementById("banner-button");
-    div.innerHTML="Have a GoodTime!"; 
-}
-function showBoxes(){
-    div = document.getElementById("container");
-    div.style.display = "flex";
-}
