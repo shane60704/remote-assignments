c3743ce75fb9e40c937fd32fdc392b676038b2ad
Sean <shane60704@gmail.com>
2024-06-30 01:44:44 +0800

    Week-2

---
+++ WeeK-2/Assignment-4/Assignment-4.js
@@ -0,0 +1,9 @@
+function changeWords()
+{
+    div = document.getElementById("banner-button");
+    div.innerHTML="Have a GoodTime!"; 
+}
+function showBoxes(){
+    div = document.getElementById("container");
+    div.style.display = "flex";
+}
